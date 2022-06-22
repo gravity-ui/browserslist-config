ddc67fcd3096b1ea0272b76e93e162e737e223ef
Valeriy Sidorenko <balepas@yandex-team.ru>
2022-06-22 17:07:16 +0300

    fix: remove environment from config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-export = {
-    production: [
-        '> 0%',
-        'not chrome < 70',
-        'not IE < 12',
-        'not kaios > 0',
-        'not edge < 19',
-        'not ff < 53',
-        'not opera < 56',
-        'not safari < 14',
-        'not samsung < 12',
-        'not ie_mob < 12',
-        'not ios_saf < 14',
-        'not android < 5',
-    ],
-};
+export = [
+    '> 0%',
+    'not chrome < 70',
+    'not IE < 12',
+    'not kaios > 0',
+    'not edge < 19',
+    'not ff < 53',
+    'not opera < 56',
+    'not safari < 14',
+    'not samsung < 12',
+    'not ie_mob < 12',
+    'not ios_saf < 14',
+    'not android < 5',
+];
