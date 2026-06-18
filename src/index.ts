3221169210d03bf619047bc1621cd9566bafdfb0
dandemchenko <dandemchenko@yandex-team.ru>
2026-06-18 18:06:16 +0200

    refactor!: Migrate to baseline rule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-export = [
-    'last 3 years and fully supports es6 and > 0.05%',
-    'not dead',
-    'not op_mini all',
-    'not and_qq > 0',
-    'not and_uc > 0',
-    'Firefox ESR',
-    'Firefox > 0 and last 3 years and > 0.01%',
-];
+export = ['baseline widely available on 2025-01-01 with downstream'];
